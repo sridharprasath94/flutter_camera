1ca8357adb4b5c8b8782ec156b020f455a8f496c
Sridhar Prasath <sridharprasath@dynamicelement.com>
2025-03-22 17:42:26 +0100

    Generate android and iOS code from pigeon file

--- packages/native_camera_controller/native_camera_controller_platform_interface/pigeon/camera_api.dart
+++ packages/native_camera_controller/native_camera_controller_platform_interface/pigeon/camera_api.dart
@@ -2,18 +2,31 @@ import 'package:pigeon/pigeon.dart';
 
 @ConfigurePigeon(
   PigeonOptions(
-    kotlinOut:
-        '../mobile_camera_controller_android/android/src/main/kotlin/com/dynamicelement/mobilecameracontroller/CameraApi.kt',
+    javaOut:
+    '../native_camera_controller_android/android/src/main/java/com/flashandroid/camera/CameraApi.java',
+    javaOptions: JavaOptions(
+      package: 'com.flashandroid.camera',
+      useGeneratedAnnotation: false,
+    ),
     dartOut: 'lib/src/channel/camera_api.pigeon.dart',
-    swiftOut: '../mobile_camera_controller_ios/ios/Classes/CameraApi.swift',
+    swiftOut: '../native_camera_controller_ios/ios/Classes/CameraApi.swift',
   ),
 )
+enum FlashState {
+  /// Flash state disabled
+  disabled,
+
+  /// Flash state enabled
+  enabled
+}
+
 @HostApi()
 abstract class CameraApi {
   void dispose();
 
   @async
-  void initialize();
+  void initialize(FlashState flashState,
+      double flashTorchLevel,);
 
   @async
   Uint8List takePicture();
@@ -25,4 +38,11 @@ abstract class CameraApi {
   void setFlashStatus({required bool isActive});
 
   bool getFlashStatus();
+}
+
+@FlutterApi()
+abstract class CameraImageListener {
+  void onImageAvailable(Uint8List image);
+
+  void onQrCodeAvailable(String qrCode);
 }
